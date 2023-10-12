7aecc236653851a758decc6d2db6f8845ee27715
Dinesh041291 <dineshkumar220555@gmail.com>
2023-10-12 17:07:38 +0530

    Dinesh folder created

---
+++ Dinesh/Dinesh.js
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Simple JavaScript Example</title>
+</head>
+
+<body>
+    <h1>Simple JavaScript Example</h1>
+    <button onclick="showAlert()">Click me!</button>
+
+    <script>
+        function showAlert() {
+            alert('Hello, World! This is a simple JavaScript alert.');
+        }
+    </script>
+</body>
+
+</html>
