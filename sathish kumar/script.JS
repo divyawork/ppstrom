2ea634eea90133298b8310c4af009dc35db26eff
SATHISH KUMAR <sathishkumarmvo@gmail.com>
2023-10-12 13:49:15 +0530

    changed my file

---
+++ sathish kumar/script.JS
@@ -0,0 +1,10 @@
+// This is a simple JavaScript code that displays a message in a web browser
+
+// Define a function that displays the message
+function showMessage() {
+  // Use the window.alert() method to show a popup message
+  window.alert("Hello, World!");
+}
+
+// Call the showMessage() function to display the message
+showMessage();
