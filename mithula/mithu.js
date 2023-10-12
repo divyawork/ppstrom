ed9cdf8fe8cb73ea6ca9731e19f5bd21bb28e148
Mithula Aishwarya B <mithulaaishwaryab@gmail.com>
2023-10-12 20:13:53 +0530

    updated

--- mithula/mithu.js
+++ mithula/mithu.js
@@ -1,17 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Simple JavaScript Example</title>
-</head>
-<body>
-    <h1>Click the button</h1>
-    <button id="myButton">Click Me</button>
-
-    <script>
-        // JavaScript code
-        document.getElementById("myButton").addEventListener("click", function() {
-            alert("Button clicked!");
-        });
-    </script>
-</body>
-</html>
+let fruits = ['apple', 'banana', 'cherry'];
+console.log(fruits[0]); // Output: 'apple'
+fruits.push('date'); // Add an element to the end
