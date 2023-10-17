dd965fd3ad346f75322dd2ec5ae26c9203a1f73b
Mithula Aishwarya B <mithulaaishwaryab@gmail.com>
2023-10-17 19:13:25 +0530

    changes

--- mithula/script.js
+++ mithula/script.js
@@ -1,27 +1,7 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const newTaskInput = document.getElementById("new-task");
-    const addTaskButton = document.getElementById("add-task");
-    const taskList = document.getElementById("tasks");
+// script.js
+const button = document.getElementById("clickMeButton");
+const output = document.getElementById("outputText");
 
-    addTaskButton.addEventListener("click", function() {
-        const taskText = newTaskInput.value.trim();
-
-        if (taskText) {
-            const listItem = document.createElement("li");
-            listItem.innerHTML = `
-                <span>${taskText}</span>
-                <button class="remove-task">Remove</button>
-            `;
-
-            taskList.appendChild(listItem);
-            newTaskInput.value = "";
-
-            const removeTaskButtons = document.querySelectorAll(".remove-task");
-            for (const button of removeTaskButtons) {
-                button.addEventListener("click", function() {
-                    taskList.removeChild(button.parentElement);
-                });
-            }
-        }
-    });
+button.addEventListener("click", function() {
+    output.textContent = "Hello from JavaScript!";
 });
