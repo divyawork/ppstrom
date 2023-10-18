ae4b4dbbbb427907bf9da624ab6d87eb425969e2
Arjunchezhiyan <arjunarjun02092@gmail.com>
2023-10-18 13:33:48 +0530

    3 folders added

---
+++ arjunchezhiyan/script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Get the button and paragraph elements by their IDs
+    var button = document.getElementById('sampleButton');
+    var text = document.getElementById('sampleText');
+
+    // Add a click event listener to the button
+    button.addEventListener('click', function() {
+        text.textContent = 'The button was clicked!';
+    });
+});
