0a93eed88667916df5d283ddc18764aecc3dee40
Arjunchezhiyan <arjunarjun02092@gmail.com>
2023-10-19 16:18:34 +0530

    index.html

--- arjun chezhiyan/script.js
+++ arjun chezhiyan/script.js
@@ -1,1 +1,9 @@
-console.log("Hello, World!");
+// script.js
+
+// Function to display a message when the button is clicked
+function displayMessage() {
+    document.getElementById("outputMessage").innerHTML = "Hello, World!";
+}
+
+// Adding an event listener to the button
+document.getElementById("myButton").addEventListener("click", displayMessage);
