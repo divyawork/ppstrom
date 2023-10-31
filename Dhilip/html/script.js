7d6508b19dca934e1908d3f9b9ab0fdb97a70e4f
DhilipSubramani <dhilip10061996@gmail.com>
2023-10-31 19:51:22 +0530

    Modified

--- Dhilip/html/script.js
+++ Dhilip/html/script.js
@@ -18,16 +18,3 @@ function calculateResult() {
         document.getElementById('display').value = 'Error';
     }
 }
-<!--
-function addNumbers() {
-    const num1 = parseFloat(document.getElementById('num1').value);
-    const num2 = parseFloat(document.getElementById('num2').value);
-
-    if (!isNaN(num1) && !isNaN(num2)) {
-        const result = num1 + num2;
-        document.getElementById('result').textContent = `Result: ${result}`;
-    } else {
-        document.getElementById('result').textContent = 'Invalid input';
-    }
-}
--->
