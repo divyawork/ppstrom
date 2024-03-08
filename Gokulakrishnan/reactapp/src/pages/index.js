1003756e44ecc7d6510c3e2584c142181e4a0a9a
Gokulakrishnan5060 <gk6050@gmail.com>
2024-03-08 22:38:10 +0530

    react js chart created

---
+++ Gokulakrishnan/reactapp/src/pages/index.js
@@ -0,0 +1,14 @@
+// src/pages/index.js
+import React from 'react';
+import ChartComponent from '../components/ChartComponent';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to My Project</h1>
+      <ChartComponent />
+    </div>
+  );
+};
+
+export default Home;
